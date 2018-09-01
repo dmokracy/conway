2f309a93be339255549ee37c1be21fff3fd2cad4
Kevin Thai <kvn.thai@gmail.com>
2018-09-01 12:46:57 -0400

    Basic UI layout and grid display

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-var canvas = document.getElementById("canvas");
+var canvas = document.querySelector(".canvas");
 var ctx = canvas.getContext("2d");
+ctx.canvas.height = window.innerHeight - 4;
+ctx.canvas.width = window.innerWidth;
+
+// Paramters for the grid
+var pixelSize = 25;
+var borderSize = 2;      // Border is drawn on the right and bottom edges of the pixel
+ctx.fillStyle = "gray";  // This will be the border colour
+ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
 // Make a grid
-for (var i = 0; i < canvas.width / 10; i++) {
-    for (var j = 0; j < canvas.width / 10; j++) {
-        ctx.fillStyle = ((i + j) % 2) ? "black" : "lightgreen";
-        ctx.fillRect(10 * i, 10 * j, 10, 10);
+for (var i = 0; i < canvas.height / pixelSize; i++) {
+    for (var j = 0; j < canvas.width / pixelSize; j++) {
+        ctx.fillStyle = ((i + j) % 70) ? "black" : "white";
+        ctx.fillRect(pixelSize * j,          pixelSize * i,
+                     pixelSize - borderSize, pixelSize - borderSize);
     }
 }
