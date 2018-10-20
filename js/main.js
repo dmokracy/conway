49402fa757837e79058421ed74982bf73057a894
Kevin Thai <kvn.thai@gmail.com>
2018-10-19 23:55:01 -0400

    Clean up Toolbar Hide animation, comment

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+/*
+ *  Grid drawing
+ */
+
 var canvas = document.querySelector(".canvas");
 var ctx = canvas.getContext("2d");
-ctx.canvas.height = window.innerHeight - 4;
+ctx.canvas.height = window.innerHeight - 4;  // slight offset or else it overflows and causes scroll in Firefox
 ctx.canvas.width = window.innerWidth;
 
-// Paramters for the grid
+// Grid parameters
 var pixelSize = 25;
 var borderSize = 2;      // Border is drawn on the right and bottom edges of the pixel
 ctx.fillStyle = "gray";  // This will be the border colour
@@ -12,38 +16,61 @@ ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 // Make a grid
 for (var i = 0; i < canvas.height / pixelSize; i++) {
     for (var j = 0; j < canvas.width / pixelSize; j++) {
-        ctx.fillStyle = ((i + j) % 70) ? "black" : "white";
+        ctx.fillStyle = ((i + j) % 70) ? "black" : "white";  // Arbitrary colouring for demo
         ctx.fillRect(pixelSize * j,          pixelSize * i,
                      pixelSize - borderSize, pixelSize - borderSize);
     }
 }
 
 
-// Hide button
+
+
+/*
+ *  Toolbar animations
+ */
+
+// Logic for hiding animation
+var hideIterator = {
+    up: {
+        style: "bottom",
+        size: "offsetHeight"
+    },
+    down: {
+        style: "top",
+        size: "offsetHeight"
+    },
+    left: {
+        style: "right",
+        size: "offsetWidth"
+    },
+    right: {
+        style: "left",
+        size: "offsetWidth"
+    }
+}
+
+// Initialize CSS attributes for hideables so CSS animations work properly
+Object.keys(hideIterator).forEach(function(dir) {
+    document.querySelectorAll(".hideable-" + dir).forEach(function(e) {
+        e.style[hideIterator[dir].style] = "0px";
+    });
+});
+
 var hideBtn = document.querySelector(".hide-button");
 hideBtn.addEventListener("click", function() {
-    var hideBtnIconClassList = hideBtn.querySelector("i").classList;
+    let hideBtnIconClassList = hideBtn.querySelector("i").classList;
     hideBtnIconClassList.toggle("fa-angle-double-up");
     hideBtnIconClassList.toggle("fa-angle-double-down");
-    document.querySelectorAll(".hide-up").forEach(function(e) {
-        if (hideBtnIconClassList.contains("fa-angle-double-down")) {
-            e.style.bottom = e.offsetHeight + "px";
-        } else {
-            e.style.bottom = 0 + "px";
-        }
-    });
-    document.querySelectorAll(".hide-down").forEach(function(e) {
-        if (hideBtnIconClassList.contains("fa-angle-double-down")) {
-            e.style.top = e.offsetHeight + "px";
-        } else {
-            e.style.top = 0 + "px";
-        }
-    });
-    document.querySelectorAll(".hide-right").forEach(function(e) {
-        if (hideBtnIconClassList.contains("fa-angle-double-down")) {
-            e.style.left = e.offsetWidth + "px";
-        } else {
-            e.style.left = 0 + "px";
-        }
+    let isHidden = hideBtnIconClassList.contains("fa-angle-double-down");
+
+    // Hide each hideable
+    Object.keys(hideIterator).forEach(function(dir) {
+        document.querySelectorAll(".hideable-" + dir).forEach(function(e) {
+            if (isHidden) {
+                e.style[hideIterator[dir].style] = e[hideIterator[dir].size] + "px";
+            } else {
+                e.style[hideIterator[dir].style] = "0px";
+            }
+        });
     });
 });
